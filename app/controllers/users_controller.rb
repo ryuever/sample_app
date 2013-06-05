7507969125e8ad9fe5f23861f7c9cd7667cf8a6d
ryuever <liuyouchao111@gmail.com>
2013-06-05 20:44:43 +0900

    add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index,:edit, :update,:destro]
+  before_filter :signed_in_user, only: [:index,:edit, :update,:destroy,:following,:followers]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
 
@@ -43,11 +43,25 @@ class UsersController < ApplicationController
     end
   end
 
-def destroy
+  def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
     redirect_to users_url
-end
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
 private
 
